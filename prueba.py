f04fd2764ac6b28594da81d1f2168c6ee0473c85
NicolasCorrea <nicolascorre93@gmail.com>
2026-07-14 23:21:36 -0400

    calculadora

--- prueba.py
+++ prueba.py
@@ -1,3 +1,25 @@
-def tuputamadre():
-    print("historico")
-tuputamadre()
+def suma(num1, num2):
+    return num1 + num2
+def resta(num1, num2):
+    return num1 - num2
+def opc():
+    opc = input("ingrese su opcion: (+, -)")
+    return opc
+def pedir_numeros():
+    num1 = int(input("Ingrese su primer numero"))
+    num2 = int(input("ingrese su segundo numero"))
+    return num1, num2
+def calculadora():
+    while True:
+        opcion = opc()
+        match opcion:
+            case "+":
+                num1, num2 = pedir_numeros()
+                print(suma(num1, num2))
+                break
+            case "-":
+                num1, num2 = pedir_numeros()
+                print(resta(num1, num2))
+                break
+                
+calculadora()
